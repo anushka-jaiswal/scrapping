24e648be8208f7dc89dbc416970776b124c451d7
anushka-jaiswal <106025966+anushka-jaiswal@users.noreply.github.com>
2022-05-31 07:53:33 +0530

    Add files via upload

---
+++ scrapning.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import csv
+URL = 'https://www.amazon.in/s?k=laptop&crid=17QXOUYI094IY&sprefix=lapto%2Caps%2C369&ref=nb_sb_noss_2'
+soup = bs('requirement.text', 'html.parser')
+titles = soup.find_all('div', attrs={'class', 'head'})
+titles_list = []
+count = 1
+for title in titles:
+    d = {}
+    d['Title Number'] = f'Title {count}'
+    d['Title Name'] = title.text
+    count += 1
+    titles_list.append(d)
+ 
+filename = 'titles.csv'
+with open(filename, 'w', newline='') as f:
+    w = csv.DictWriter(f,['Title Number','Title Name'])
+    w.writeheader()
+     
+    w.writerows(titles_list)
